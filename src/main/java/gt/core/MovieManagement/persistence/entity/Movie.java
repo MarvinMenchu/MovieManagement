f4db9ea79f44465ef1856a513313740824d21bb2
Ing. Marvin Menchu <marvin.menchu@icloud.com>
2024-10-15 17:02:47 -0600

    LOGRO: COMPRESION Y USO DE JSON MANAGED REFERENCE Y JSON BACK REFERENCE

--- src/main/java/gt/core/MovieManagement/persistence/entity/Movie.java
+++ src/main/java/gt/core/MovieManagement/persistence/entity/Movie.java
@@ -1,14 +1,20 @@
 package gt.core.MovieManagement.persistence.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import gt.core.MovieManagement.util.MovieGenre;
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
 public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
 
     @Column(nullable = false)
@@ -18,10 +24,20 @@ public class Movie {
     private String director;
 
     @Enumerated(EnumType.STRING)
+    @JsonProperty(access = JsonProperty.Access.READ_WRITE)
     private MovieGenre genre;
+
+    @JsonProperty(value = "release-year")
     private int releaseYear;
 
+    @CreationTimestamp
+    @JsonProperty(value = "created-at")
+    @JsonFormat(pattern = "yyyy/MM/dd - HH:mm:ss")
+    @Column(updatable = false, columnDefinition = "TIMESTAMP DEFAULT NOW()")
+    private LocalDateTime createdAt;
+
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "movie")
+    @JsonManagedReference("movie-to-rating")
     private List<Rating> ratings;
 
     public Long getId() {
@@ -62,6 +78,14 @@ public class Movie {
 
     public void setReleaseYear(int releaseYear) {
         this.releaseYear = releaseYear;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 
     public List<Rating> getRatings() {
--- src/main/java/gt/core/MovieManagement/persistence/entity/Rating.java
+++ src/main/java/gt/core/MovieManagement/persistence/entity/Rating.java
@@ -1,5 +1,8 @@
 package gt.core.MovieManagement.persistence.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import org.hibernate.annotations.Check;
 
@@ -7,20 +10,27 @@ import org.hibernate.annotations.Check;
 public class Rating {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
 
     @Column(name = "movie_id", nullable = false)
+    @JsonProperty("movie-id")
     private Long movieId;
 
     @Column(name = "user_id", nullable = false)
+    @JsonProperty("user-id")
     private Long userId;
 
     @ManyToOne
     @JoinColumn(name = "movie_id", insertable = false, updatable = false)
+    //@JsonIgnore
+    @JsonBackReference("movie-to-rating")
     private Movie movie;
 
     @ManyToOne
     @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    //@JsonIgnore
+    @JsonBackReference("user-to-rating")
     private User user;
 
     @Check(constraints = "rating >= 0 AND rating <= 5")
